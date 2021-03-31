f0f05bc5f350fce60d74245cd086b8a2cc9f6640
akash084 <akashbaniya084@gmail.com>
2021-03-31 14:26:40 +0545

    Added blog v-1

---
+++ Backend/login/loginapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('register/', views.registerPage, name="register"),
+    path('', views.loginPage, name="login"),
+    path('logout/', views.logoutUser, name="logout"),
+    path('home/', views.homePage, name="home"),
+    # path('blog/', views.Blog, name="Blog"),
+]
