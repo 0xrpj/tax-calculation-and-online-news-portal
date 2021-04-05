6b807d34b9a3a368ab90aa71217f7f384e12e10f
akash084 <akashbaniya084@gmail.com>
2021-04-05 21:15:09 +0545

    Added add in dashboard

---
+++ Backend/login/loginapp/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('dashboard')
+
+class Post(models.Model):
+    title = models.CharField(max_length = 255)
+    auth = models.ForeignKey(User,on_delete = models.CASCADE)
+    body = models.TextField()
+    category = models.CharField(max_length=255, default='Politics')
+
+    def __str__(self):
+        return self.title + ' | ' + str(self.auth)
+    
+    def get_absolute_url(self):
+        return reverse('dashboard')
---
+++ Backend/login/loginapp/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+from django.contrib.auth import authenticate, login, logout 
+from .models import Post
+from .forms import CreateUserForm, PostForm
+
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView , DetailView, CreateView
+
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for '+user)
+                return redirect('login')
+
+        context = {'form': form}
+        return render(request, 'loginapp/Html file/Register.html', context)
+
+
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('login')
+    else:
+
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.info(request, 'Email OR password is incorrect')
+                return redirect('login')
+        context = {}
+        return render(request, 'loginapp/Html file/SignIn.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
+def homePage(request):
+    context = {}
+    return render(request, 'loginapp/Html file/index.html', context)
+
+class Blog(ListView):
+    model = Post
+    template_name = 'loginapp/Html file/blog.html'
+
+class Detail_Article_View(DetailView):
+    model = Post
+    template_name = 'loginapp/Html file/detailed_article.html'
+
+class AddPostView(CreateView, ListView):
+    model = Post
+    form_class = PostForm
+    template_name = 'loginapp/Html file/Dashboard.html'
+    # fields = '__all__'
